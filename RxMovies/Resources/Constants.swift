448136c0cde3235ea53550d26b2005989920d94f
Eduardo Jordan <eduardojordan@hotmail.com>
2021-03-29 12:43:48 +0200

    constants

---
+++ RxMovies/Resources/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  RxMovies
+//
+//  Created by Eduardo Jordan on 29/3/21.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = ""
+    
+    struct URL {
+        static let main = "https://api.themoviedb.org"
+    }
+    
+    struct Endpoints {
+        static let urlListPopularMovies = "3/movie/popular"
+        static let urlImage = "https:// image.tmdb.org/t/p/w200"
+        static let urlDetailMovie = "3/movie"
+    }
+}
